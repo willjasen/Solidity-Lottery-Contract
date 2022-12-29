34142d6093769afea2c852065941874f3be9d7d8
nawodyaishan <nawodyain@gmail.com>
2022-12-29 15:12:08 +0530

    Add multiple account test

--- test/Lotery.test.js
+++ test/Lotery.test.js
@@ -42,7 +42,27 @@ describe(`Lottery Contract`, () => {
         assert.equal(1, players.length);
     });
 
-    it('should ', async () => {
+    it('should multiple accounts to enter ', async () => {
 
+        // Entering 3 Accounts
+        await lottery.methods.enter().send({
+            from: accounts[0], value: web3.utils.toWei(`0.02`, "ether")
+        });
+        await lottery.methods.enter().send({
+            from: accounts[1], value: web3.utils.toWei(`0.02`, "ether")
+        });
+        await lottery.methods.enter().send({
+            from: accounts[2], value: web3.utils.toWei(`0.02`, "ether")
+        });
+
+        // Calling getPlayers from [0] account
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length);
     });
 })
