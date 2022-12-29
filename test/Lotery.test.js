4d3da37112b53e05b6e3ca3631a2ca74b77220c7
nawodyaishan <nawodyain@gmail.com>
2022-12-29 15:38:46 +0530

    Add full end-to-end test

--- test/Lotery.test.js
+++ test/Lotery.test.js
@@ -95,5 +95,26 @@ describe(`Lottery Contract`, () => {
 
     it('should send money to the winner and reset players array', async () => {
 
+        await lottery.methods.enter().send({
+            from: accounts[0], value: web3.utils.toWei(`2`, `ether`)
+        })
+
+        // Must check that reduced 2 ethers
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+        await lottery.methods.pickWinner().send({
+            from: accounts[0]
+        })
+
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+        console.log(initialBalance);
+        console.log(finalBalance);
+
+        const difference = finalBalance - initialBalance;
+        console.log(difference);
+
+        // Checking balance with reduced gas amount
+        assert(difference > web3.utils.toWei(`1.8`, `ether`))
     });
 })
